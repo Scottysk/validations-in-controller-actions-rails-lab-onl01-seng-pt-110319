227e093b07bd4f7a2642c7191e420d99a34c4ba4
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-11 03:03:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   
   validates :title, presence: true
-  valides :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates(:content, { :length => })
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :content, length: { minimum: 100 }
 end
